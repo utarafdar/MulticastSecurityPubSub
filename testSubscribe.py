d1a564edd7d0cce8aab1c213060c2933e3e738f6
utarafdar <umair.tarafdar@gmail.com>
2019-02-09 01:10:23 +0100

    test commit

--- testSubscribe.py
+++ testSubscribe.py
@@ -3,7 +3,7 @@ import paho.mqtt.subscribe as subscribe
 def print_msg(client, userdata, message):
     print(message.topic, str(message.payload.decode("utf-8")))
 
-# loops forever
+# loops forever test commit
 print("before callback")
 subscribe.callback(print_msg, "thesis/progress", hostname="iot.eclipse.org")
 print("after callback")
